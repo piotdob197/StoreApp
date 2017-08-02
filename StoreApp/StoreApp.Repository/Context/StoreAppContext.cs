11164244e56e65191ff52207c0e8d1cb483993f8
Piotrek <piotr.dobija94@gmail.com>
2017-08-02 20:16:17 +0200

    #StoreApp przeniesienie modelu do nowego projektu

---
+++ StoreApp/StoreApp.Repository/Context/StoreAppContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using StoreApp.Repository.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace StoreApp.Context
+{
+
+
+    public class StoreAppContext : IdentityDbContext
+    {
+        public StoreAppContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static StoreAppContext Create()
+        {
+            return new StoreAppContext();
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+    }
+}
